139c66e1a925387cea847249dee6296cab3a48d5
Blake Richey <spsprogrammer@SPSs-Mac-mini.local>
2019-05-16 08:53:48 -0500

    Added new daily coding challenge

---
+++ Python/Daily Coding Challenges/33.py
@@ -0,0 +1,20 @@
+"""Compute the running median of a sequence of numbers. That is, given a stream of numbers, print out the median of the list so far on each new element."""
+class RunningMedian():
+  def __init__(self):
+    self.vals=[]
+    self.size=0
+  
+  def add(self,newVal):
+    self.vals.append(newVal)
+    self.vals=sorted(self.vals)
+    self.size+=1
+    length=self.size
+    if length%2:
+      print((self.vals[int(length/2)]))
+    else:
+      index=int(length/2)
+      print((self.vals[index]+self.vals[index-1])/2)
+L=[2, 1, 5, 7, 2, 0, 5]
+start=RunningMedian()
+for x in L:
+  start.add(x)
